32a9756886f1f6c115542635ac43853ca1602e34
sudhir <sudhir.madaan@ogilvy.com>
2025-08-04 17:56:54 +0530

    Remove package-lock.json, refactor layout component to eliminate mobile navigation code, and update asset manifest with new static file references for improved organization and maintainability.

--- components/layout.js
+++ components/layout.js
@@ -14,8 +14,6 @@
  */
 
 import { usePathname } from "next/navigation";
-import { Disclosure } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Footer from './Footer';
 import Header from './PageHeader';
 
@@ -33,44 +31,7 @@ export default function Layout({ children }) {
   
   return (
     <div className="min-h-full">
-      <Disclosure as="nav" className="bg-gray-100">
-        {({ open }) => (
-          <>
-            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-              <div className="relative flex items-center justify-between">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                  {/* Mobile menu button*/}
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-800 hover:text-gray-700 hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-
-                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                  <div className="flex-shrink-0 flex items-center">
-          
-                  </div>
-                  <div className="hidden sm:block sm:ml-6">
-                    <div className="flex space-x-4">
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <Disclosure.Panel className="sm:hidden">
-              <div className="px-2 pt-2 pb-3 space-y-1">
-                
-              </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
-      {/* <Header /> */}
+      <Header />
       <main>{children}</main>
       <Footer />
     </div>
