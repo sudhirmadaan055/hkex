87e429c17d40873227879fca17dce30343b36286
sudhir <sudhir.madaan@ogilvy.com>
2025-08-04 17:21:32 +0530

    Remove unnecessary height specification from layout header for improved responsiveness and layout consistency.

--- components/layout.js
+++ components/layout.js
@@ -37,7 +37,7 @@ export default function Layout({ children }) {
         {({ open }) => (
           <>
             <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-              <div className="relative flex items-center justify-between h-16">
+              <div className="relative flex items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   {/* Mobile menu button*/}
                   <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-800 hover:text-gray-700 hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
