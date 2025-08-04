9f4e15e45e44e13ca6e9e9ac9dec768ad93c836a
AvinashKumarRay <avinashkumar.ray@pennywisesolutions.com>
2025-08-04 15:12:40 +0530

    Add header component and assets for navigation
    - Introduced a new Header component that fetches and displays the HKEX header data, including the company logo and main navigation links.
    - Added new SVG icons for notifications and search functionalities.
    - Included a new logo image for branding.
    - Updated asset manifest to include new routes and client reference manifests for teams and not-found pages.

--- app/[lang]/groups/page.js
+++ app/[lang]/groups/page.js
@@ -16,6 +16,7 @@ import Link from 'next/link';
 import aemHeadlessClient from '../../../lib/aem-headless-client';
 import TeamImage from '../../../components/TeamImage';
 import NewsCarousel from '../../../components/NewsCarousel';
+import Header from '../../../components/Header';
 // import {getAuthorHost, getProtocol, getService} from "../../../lib/fetch-data";
 
 
@@ -107,6 +108,7 @@ export default async function Teams() {
     if (!teams.length) {
       return (
         <Layout>
+          <Header/>
           <div className="container-fluid bg-light">
             <NewsCarousel />
           </div>
