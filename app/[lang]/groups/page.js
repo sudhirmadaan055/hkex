420341d8a3627d41a3e811a595b697e3f8905078
Nikhil Seth <nikhil.seth@verticurl.com>
2025-08-04 19:57:04 +0530

    Adding variation for news carousel

--- app/[lang]/groups/page.js
+++ app/[lang]/groups/page.js
@@ -27,7 +27,8 @@ export default async function Teams() {
             <HeroBanner />
           </div>
           <div className="container-fluid bg-light">
-            <NewsCarousel />
+            <NewsCarousel cfPath="/content/dam/my-project/en/hkex-carousel"/>
+            <NewsCarousel cfPath="/content/dam/my-project/en/latest-hkex-carousel" variation="text-blue"/>
             <Community />
           </div>
         </Layout>
