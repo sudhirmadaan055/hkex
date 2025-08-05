16213d6cc026e3044e94a30d2edf2fe10f43e821
sudhir <sudhir.madaan@ogilvy.com>
2025-08-05 12:07:26 +0530

    Refactor HeroBanner component to simplify image source path and update asset-manifest.json with new file references for improved build management.

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -15,7 +15,7 @@ const HeroBanner = async (props) => {
     <div className="relative w-full" {...editorProps}>
       {/* Hero Image */}
       <img
-        src={heroBannerItems.heroBannerItems[0]?.heroBannerImage}
+        src={heroBannerItems.heroBannerImage}
         alt="Hero Banner"
         className="w-full aspect-[1440/700] object-cover"
         data-aue-prop="image" data-aue-type="media"
